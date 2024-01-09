28b5d2bf3fab356f17d28286cb5c4f62cc07bec3
Willard Lu <willard.lu@outlook.com>
2024-01-09 20:14:03 +0800

    Add file header comments

--- cmd/main/sunflower.go
+++ cmd/main/sunflower.go
@@ -1,3 +1,10 @@
+// @copyright Copyright 2024 Willard Lu
+// @email willard.lu@outlook.com
+// @language go 1.18.1
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
 package main
 
 import (
