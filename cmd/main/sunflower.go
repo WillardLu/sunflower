d3a203182e0c39c3596023e09ea83e399d040943
Lu Wei <willard.lu@outlook.com>
2024-01-09 20:15:05 +0800

    update

--- cmd/main/sunflower.go
+++ cmd/main/sunflower.go
@@ -7,31 +7,9 @@
 // https://opensource.org/licenses/MIT.
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-)
-
-/*
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "belighT928s"
-	dbname   = "youling"
-)*/
+import "sunflower/internal/sunflowerwebserver"
 
 func main() {
-	/*
-		var err error
-		psqlconn := fmt.Sprintf("host=%s port=%d user=%s "+
-			"password=%s dbname=%s sslmode=disable",
-			host, port, user, password, dbname)
-		db, err = sql.Open("postgres", psqlconn)
-		if err != nil {
-			log.Fatal(err)
-		}*/
-
-	router := gin.Default()
-	router.Static("/", "./static")
-	router.Run("192.168.0.104:8080")
+	router := sunflowerwebserver.SetupRouter()
+	router.Run(":8080")
 }
